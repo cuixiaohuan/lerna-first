242413fb89a914a8ef32834ef06adde80351e75c
‘cuixiaohuan’ <479083227@qq.com>
2024-03-27 14:13:36 +0800

    init project

---
+++ src/print.js
@@ -0,0 +1,6 @@
+import data from "../assets/data.json";
+
+export default () => {
+    console.log('this is something about printing');
+    console.log("%c "+_.join(['Hello', 'webpack'], `' ${data.aaa} '`), "color:#008000;");
+};
